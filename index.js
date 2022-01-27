995d56baaf6fffced6a98bd527941a94071fcce3
Shivam Pandey <pandyaksbm@gmail.com>
2022-01-27 23:23:14 +0530

    final push hopefully

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const fs = require('fs');
 const bodyParser = require('body-parser');
 const app = express();
 const multer = require('multer');
-var upload = multer({ dest: 'uploads/' })
+var upload = multer()
 const csv = require('csv-parser');
 const cors = require('cors');
 
@@ -20,12 +20,6 @@ app.options('*', cors())
 
 
 
-// app.use(function (req, res, next) {
-//     res.header("Access-Control-Allow-Origin", "*");
-//     res.header("Access-Control-Allow-Headers", "Origin , X-Requested-With , Content-Type , Accept");
-//     res.header("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE,PUT");
-// })
-
 
 app.post('/hello', upload.array('file'), async (req, res, next) => {
     console.log('i got req')
@@ -34,22 +28,53 @@ app.post('/hello', upload.array('file'), async (req, res, next) => {
         res.sendStatus(450);
         return;
     }
+
     let old_data = [];
     let new_data = [];
 
-    var csvPipe1 = await fs.createReadStream(req.files[0].path).pipe(csv());
-    var csvPipe2 = await fs.createReadStream(req.files[1].path).pipe(csv());
+    let a = await req.files[0].buffer.toString();
+    let b = await req.files[1].buffer.toString();
+    let c = await a.split('\n');
+    let c2 = await b.split('\n');
 
-
-
-    for await (let row of csvPipe1) {
-        await old_data.push(row["Cell Number"]);
-        // console.log(row)
+    for await (let d of c) {
+        let appu = await d.split(',');
+        let num = await appu[0];
+        old_data.push(parseInt(num));
     }
-    for await (let row of csvPipe2) {
-        await new_data.push(row["Cell Number"]);
-        // console.log(row)
+
+    for await (let d of c2) {
+        let appu = await d.split(',');
+        let num = await appu[0];
+        new_data.push(parseInt(num));
     }
+
+    // console.log(typeof (a));
+
+    // console.log(a, b, c);
+
+    // var matches1 = await a.match('/^\d{10}$/');
+    // var matches2 = await b.match('/^\d{10}$/');
+
+    // console.log(matches1, matches2);
+
+
+
+
+
+    // var csvPipe1 = await fs.createReadStream(req.files[0].path).pipe(csv());
+    // var csvPipe2 = await fs.createReadStream(req.files[1].path).pipe(csv());
+
+
+
+    // for await (let row of csvPipe1) {
+    //     await old_data.push(row["Cell Number"]);
+    //     // console.log(row)
+    // }
+    // for await (let row of csvPipe2) {
+    //     await new_data.push(row["Cell Number"]);
+    //     // console.log(row)
+    // }
 
     const filteredArray = await old_data.filter(value => new_data.includes(value));
     let difference = await new_data.filter(x => !filteredArray.includes(x));
